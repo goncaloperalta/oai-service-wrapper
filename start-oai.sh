d59546552dfe9b0de17abd86951ffd85374bee41
gonçalo <goncaloperalta@ua.pt>
2026-03-03 14:38:49 +0000

    fixing paths

--- start-oai.sh
+++ start-oai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/user/openairinterface5g/cmake_targets/ran_build/build
+cd /home/goncalo/openairinterface5g/cmake_targets/ran_build/build
 sudo ./nr-uesoftmodem -C 765500000 --CO -55000000 -r 25 --numerology 0 --band 28 --ssb 53 --rfsim
