7c2f4fd65c7a641685c020d349a2b85c54012245
gonçalo <goncaloperalta@ua.pt>
2026-03-03 14:30:01 +0000

    formating

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ main(){
     chmod +x start-oai.sh
     cp start-oai.sh /usr/local/bin/
     cp oai-ue.service /etc/systemd/system/
-    
+
     systemctl daemon-reload
     systemctl enable oai-ue.service
     systemctl start oai-ue.service
