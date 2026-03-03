792fa0b278bbb88bb1430c644c9e1c6cad261477
gonçalo <goncaloperalta@ua.pt>
2026-03-03 13:59:35 +0000

    fix in start script

--- start-oai.sh
+++ start-oai.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/user/openairinterface5g/cmake_targets/ran_build/build
-3 sudo ./nr-uesoftmodem -C 765500000 --CO -55000000 -r 25 --numerology 0 --band 28 --ssb 53 --rfsim
+sudo ./nr-uesoftmodem -C 765500000 --CO -55000000 -r 25 --numerology 0 --band 28 --ssb 53 --rfsim
