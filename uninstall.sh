f5185a851768574ddf420a4ad54f33f73998f20d
gonçalo <goncaloperalta@ua.pt>
2026-03-03 14:43:23 +0000

    fix uninstall script path

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ main(){
     systemctl stop oai-ue.service
     systemctl daemon-reload
 
-    rm -f /usr/local/bin/start-oai
+    rm -f /usr/local/bin/start-oai.sh
     rm -f /etc/systemd/system/oai-ue.service
 }
 
