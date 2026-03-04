14b71b5ab5948778977b3b0e68b8a75967a23bff
gonçalo <goncaloperalta@ua.pt>
2026-03-04 16:50:18 +0000

    add new check for oai tunnel and allow only for run as sudo

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 main(){
+    if ! [ $(id -u) = 0 ]; then
+       echo "Must run as root"
+       exit 1
+    fi
+
     chmod +x start-oai.sh
     cp start-oai.sh /usr/local/bin/
     cp oai-ue.service /etc/systemd/system/
+
+    chmod +x check-tunnel.sh
+    cp check-tunnel.sh /usr/local/bin/
+    (crontab -l ; echo "*/1 * * * * /usr/local/bin/check-tunnel.sh") | crontab -
 
     systemctl daemon-reload
     systemctl enable oai-ue.service
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 main(){
+    if ! [ $(id -u) = 0 ]; then
+       echo "Must run as root"
+       exit 1
+    fi
+    
     systemctl disable oai-ue.service
     systemctl stop oai-ue.service
     systemctl daemon-reload
 
+    crontab -l | grep -v "/usr/local/bin/check-tunnel.sh" | crontab -
+    
     rm -f /usr/local/bin/start-oai.sh
+    rm -f /usr/local/bin/check-tunnel.sh
     rm -f /etc/systemd/system/oai-ue.service
 }
 
