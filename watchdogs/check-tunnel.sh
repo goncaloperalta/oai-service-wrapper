96941c0329307e0939c3a1b73bd11aa4eb506a73
gonçalo <goncaloperalta@ua.pt>
2026-03-08 17:34:13 +0000

    fix wrong ip for core network

--- watchdogs/check-tunnel.sh
+++ watchdogs/check-tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 main(){
-    if ! { ip link show oaitun_ue1 && ping -I oaitun_ue1 -c 1 10.0.0.5; }; then
+    if ! { ip link show oaitun_ue1 && ping -I oaitun_ue1 -c 1 15.0.0.1; }; then
         systemctl kill -s SIGTERM oai@ue.service
         /usr/local/bin/oai-stats.sh tunnel_missing
     fi
